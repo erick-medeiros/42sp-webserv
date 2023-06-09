db7a33c2e90b2927d8968cdf9dd8d3e2d7164ec8
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-06-09 17:50:27 -0300

    test: add more tests

--- src/Cookie_test.cpp
+++ src/Cookie_test.cpp
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 14:50:36 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/06/09 15:06:11 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/06/09 17:49:54 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,18 @@ TEST_SUITE("Class Cookie")
 	{
 		Cookie cookie;
 		CHECK_NE(cookie.generateSession(), cookie.generateSession());
+	}
+	TEST_CASE("set and get")
+	{
+		Cookie      cookie;
+		std::string key("test");
+		std::string key2("test2");
+
+		cookie.set(key, "123");
+		CHECK_EQ(cookie.get(key), "123");
+		cookie.set(key2, "hello");
+		CHECK_EQ(cookie.get(key2), "hello");
+		cookie.set(key, "1234");
+		CHECK_EQ(cookie.get(key), "1234");
 	}
 }
