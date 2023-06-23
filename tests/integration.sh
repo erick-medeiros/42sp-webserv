f1607d4d695849e3e118529e6a44a88f182d8b3a
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-06-23 12:21:05 -0300

    test: automatic test discovery

--- tests/integration.sh
+++ tests/integration.sh
@@ -5,21 +5,60 @@ TEST_FOLDER=tests/integration/
 
 run() {
 	TEST=$1
-	PY="$TEST_FOLDER""$TEST"/test.py
-	CONF="$TEST_FOLDER""$TEST"/config.conf
-	./$BIN ./$CONF 1>/dev/null 2>/dev/null &
+	DEBUG=$2
+	echo -n "TEST: $TEST "
+	if [ ! -d $TEST ]; then
+		echo ❌
+		exit
+	fi
+	PY="$TEST"/test.py
+	CONF="$TEST"/config.conf
+	if [ $DEBUG = true ]; then
+		./$BIN ./$CONF &
+	else
+		./$BIN ./$CONF 1>/dev/null 2>/dev/null &
+	fi
 	PID=$!
 	export PYTHONPATH=$PYTHONPATH:$(pwd)/$TEST_FOLDER
 	python3 $PY
 	EXITCODE=$?
 	kill $PID
-	echo -n "TEST:" $TEST " "
-	if [ $EXITCODE != 0 ]; then echo ❌; exit $EXITCODE; fi
+	if [ $EXITCODE != 0 ]; then
+		echo ❌
+		exit $EXITCODE
+	fi
 	echo ✅
 	return $EXITCODE
 }
 
-run "status_code"
-run "multiple_servers"
-run "default_error_page"
-run "limit_client_body_size"
+folders() {
+	for f in "$1"*; do
+		if [ -d "$f" ]; then
+			if [ -z $(echo $f | grep __pycache__) ]; then
+				echo $f
+			fi
+		fi
+	done
+}
+
+subfolders() {
+	for f in $(folders $1); do
+		folders "$f"/
+	done
+}
+
+run_all_tests() {
+	for f in $(folders $TEST_FOLDER); do
+		run $f false
+	done
+}
+
+run_single_test() {
+	run $TEST_FOLDER$1 true
+}
+
+if [ -n "$1" ]; then
+	run_single_test $1
+else
+	run_all_tests
+fi
