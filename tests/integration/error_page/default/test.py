d995f0235673d989d1a40455f1e64e918f12e9a1
Lucas <lucasalexkuhn@gmail.com>
2023-06-25 00:38:57 +0000

    fix default error page test

--- tests/integration/error_page/default/test.py
+++ tests/integration/error_page/default/test.py
@@ -3,8 +3,7 @@ import requests
 import integration
 
 URL = "http://localhost:8080/missing_page"
-error_page = "error_pages/404.html"
 
 response = integration.get(URL)
-expected_body = open(error_page).read()
+expected_body = "<html><head><title>404</title><style>body{display:grid;place-content:center;margin:0;text-align:center;height:100vh}code{font-size:3em;background:#ff2a5130;border-radius:.2em}</style></head><body><code>404</code><h2>Not Found</h2></body></html>"
 assert response.text == expected_body
