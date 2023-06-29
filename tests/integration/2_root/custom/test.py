f21f9e70ef6bfee190d369a0101b9ba08db638de
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-06-29 11:53:00 -0300

    test: add more tests for config root

--- tests/integration/2_root/custom/test.py
+++ tests/integration/2_root/custom/test.py
@@ -4,15 +4,15 @@ import integration
 
 URL_1 = "http://localhost:9000/index.html"
 URL_2 = "http://localhost:9001/index.html"
+URL_3 = "http://localhost:9002/index.html"
+URL_4 = "http://localhost:9003/index.html"
 
-response = integration.get(URL_1)
+def check(URL):
+	response = integration.get(URL)
+	assert response.status_code == 200
+	assert "root path" in response.text
 
-assert response.status_code == 200
-
-assert "root path" in response.text
-
-response = integration.get(URL_2)
-
-assert response.status_code == 200
-
-assert "root path" in response.text
+check(URL_1)
+check(URL_2)
+check(URL_3)
+check(URL_4)
